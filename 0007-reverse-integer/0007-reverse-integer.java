15d39879ba3fd6e56dc0e192a5ace86a038d1e8d
Vara Anjan B <168795718+Vara-Anjan-B@users.noreply.github.com>
2025-03-13 07:15:43 +0530

    Time: 1 ms (83.64%), Space: 41 MB (56.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,19 +1,10 @@
 class Solution {
     public int reverse(int x) {
-    if(x<Math.pow(2,31)-1 && x>Math.pow(-2,31)){
-        int x2 = x;
-        int b = 0;
-        int x3;
-        if(x>0){
-            x3 = x;
-        }else{
-           x3 = -1*x;
-        }
-        int count = 0;
-        while(x3>0){
-            x3 /= 10;
-            count++;
-        }
+        int x2 = x,b = 0,x3;
+        if(x>0) x3 = x;
+        else x3 = -1*x;
+        String z = Integer.toString(x3);
+        int count = z.length();
        if(x!=0){
            for(int i=0;i<count;i++){
                int a = x2%10;
@@ -26,9 +17,5 @@ class Solution {
        }else{
            return 0;
        }
-    }
-    else{
-        return 0;
-        }
     }
 }
